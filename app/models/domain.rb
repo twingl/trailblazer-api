631531988e9e03564975a1930a23081d61a0dd5c
Greg Signal <greg@awesome.org.nz>
2014-06-30 12:51:43 +1200

    Add convenience method for :imported?

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,4 +2,8 @@ class Domain < ActiveRecord::Base
   has_many :domain_admin_roles
   has_many :admins, :through => :domain_admin_roles, :source => :user
   has_many :org_units
+
+  def imported?
+    imported_at.present?
+  end
 end
