1366b01e317221f7885c632a113f0b22b7d85624
Greg Signal <greg@awesome.org.nz>
2014-07-18 15:09:52 +1200

    Reuse existing titles if we have them

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,4 +2,15 @@ class Node < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :user
   belongs_to :parent, :class_name => "Node"
+
+  before_save :get_title
+
+private
+
+  def get_title # Grab the title from another node if present
+    node = Node.where("title IS NOT NULL").find_by(:url => url)
+    if node
+      self.title = node.title
+    end
+  end
 end
