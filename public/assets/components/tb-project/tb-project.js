4f7a257c15084109da291d81aba82e4aeecd7eda
Zygar <a@andywilkinson.co.nz>
2014-07-19 21:19:13 +1200

    Adds deletion of projects

--- public/assets/components/tb-project/tb-project.js
+++ public/assets/components/tb-project/tb-project.js
@@ -1,4 +1,11 @@
 
 Polymer('tb-project', {
-  ready: function() {}
+  ready: function() {},
+
+  deleteProject: function(e) {
+    console.log(e, this.project)
+    id = this.project.id;
+    this.shadowRoot.querySelector('core-ajax#delete-project').go();
+
+  }
 });
