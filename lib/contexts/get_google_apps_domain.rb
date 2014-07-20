f0c2da8b8d6f74fed03c1a02f28a04837794613b
Greg Signal <greg@awesome.org.nz>
2014-07-20 19:07:29 +1200

    Verify ownership of specific domain
    Models would erroneously be created before if an arbitrary domain was
    specified as the URL parameter.

--- lib/contexts/get_google_apps_domain.rb
+++ lib/contexts/get_google_apps_domain.rb
@@ -12,7 +12,7 @@ class GetGoogleAppsDomain < Playhouse::Context
     client.fetch_access_token!
 
     # Fetch a list of the users in this domain
-    response = client.directory_users_list
+    response = client.directory_users_list(:domain => domain_name)
 
     if response.success?
       domain = Domain.find_or_create_by(:domain => domain_name) do |d|
