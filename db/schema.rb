081dcb35b672042145e9ec61ce0a88f8e6f3b3d0
Greg Signal <greg@awesome.org.nz>
2014-09-22 15:16:02 +1200

    Add unique constraint on email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916032438) do
+ActiveRecord::Schema.define(version: 20140922031451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -175,6 +175,7 @@ ActiveRecord::Schema.define(version: 20140916032438) do
   end
 
   add_index "users", ["domain_id"], name: "index_users_on_domain_id", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["org_unit_id"], name: "index_users_on_org_unit_id", using: :btree
 
 end
