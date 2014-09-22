b68c43098bfb697800ae900deb2ffc0e99bba53d
Greg Signal <greg@awesome.org.nz>
2014-09-22 16:59:02 +1200

    Allow rank parameter in Node endpoints

--- app/controllers/api/v1/nodes_controller.rb
+++ app/controllers/api/v1/nodes_controller.rb
@@ -57,7 +57,7 @@ module Api::V1
     end
 
     def node_params
-      params.require(:node).permit(:url, :title, :arrived_at, :departed_at, :idle, :parent_id)
+      params.require(:node).permit(:url, :title, :arrived_at, :departed_at, :idle, :parent_id, :rank)
     end
 
     def coord_params
