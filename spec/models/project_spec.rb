f255719654d8bc023adaaad467e73f0989b59164
Greg Signal <greg@awesome.org.nz>
2014-09-24 16:50:31 +1200

    Mark pending specs for Project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Project, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has accessors for related models" do
+    expect(subject).to belong_to(:classroom)
+    expect(subject).to have_many(:assignments).dependent(:destroy)
+    expect(subject).to have_many(:users).through(:assignments)
+  end
+
+  describe "#assign" do
+    pending "it creates an assignment for each of the assignees"
+  end
+
+  describe "#assign_to" do
+    pending "it should delegate to #assign"
+  end
 end
