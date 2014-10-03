f7b8115f39fd29b7d981558ca74f642ca9c3943c
Greg Signal <greg@awesome.org.nz>
2014-10-03 16:59:14 +1300

    Add token and visibility fields to Assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,10 +7,22 @@ class Assignment < ActiveRecord::Base
 
   before_create :set_title_and_description
 
+  before_save :set_token
+
   # This shouldn't be created directly - there is a helper method
   # `Project#assign` which accepts an array of users to achieve this.
 
   private
+
+  def set_token
+    if self.public_url_token.blank?
+      token = loop do
+        random_token = SecureRandom.urlsafe_base64(nil, false)
+        break random_token unless Assignment.exists?(:public_url_token => random_token)
+      end
+      self.public_url_token = token
+    end
+  end
 
   def set_title_and_description
     self.title       = project.title
