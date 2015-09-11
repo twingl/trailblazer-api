17bad4fcb7a44cef6fb9f091e85a5463af0faef0
Greg Signal <greg@awesome.org.nz>
2015-09-11 17:13:20 +1200

    Applications may be trusted and skip approval

---
+++ db/migrate/20150911050919_add_trusted_app_to_oauth_applications.rb
@@ -0,0 +1,5 @@
+class AddTrustedAppToOauthApplications < ActiveRecord::Migration
+  def change
+    add_column :oauth_applications, :trusted, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,47 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302221132) do
+ActiveRecord::Schema.define(version: 20150911050919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "assignment_backups", force: true do |t|
+  create_table "assignment_backups", force: :cascade do |t|
     t.integer "user_id"
     t.text    "backup"
   end
 
   add_index "assignment_backups", ["user_id"], name: "index_assignment_backups_on_user_id", using: :btree
 
-  create_table "assignments", force: true do |t|
+  create_table "assignments", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "project_id"
-    t.string   "title"
+    t.string   "title",            limit: 255
     t.datetime "completed_at"
     t.datetime "started_at"
     t.integer  "current_node_id"
     t.text     "description"
-    t.string   "temp_id"
+    t.string   "temp_id",          limit: 255
     t.text     "public_url_token"
-    t.boolean  "visible",          default: false
+    t.boolean  "visible",                      default: false
   end
 
   add_index "assignments", ["current_node_id"], name: "index_assignments_on_current_node_id", using: :btree
   add_index "assignments", ["project_id", "user_id"], name: "index_assignments_on_project_id_and_user_id", unique: true, using: :btree
   add_index "assignments", ["user_id", "project_id"], name: "index_assignments_on_user_id_and_project_id", unique: true, using: :btree
 
-  create_table "classrooms", force: true do |t|
-    t.string   "name"
+  create_table "classrooms", force: :cascade do |t|
+    t.string   "name",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "domain_id"
     t.text     "description"
-    t.string   "group_key"
+    t.string   "group_key",   limit: 255
   end
 
   add_index "classrooms", ["domain_id"], name: "index_classrooms_on_domain_id", using: :btree
 
-  create_table "classrooms_users", force: true do |t|
+  create_table "classrooms_users", force: :cascade do |t|
     t.integer "user_id"
     t.integer "classroom_id"
   end
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20150302221132) do
   add_index "classrooms_users", ["classroom_id", "user_id"], name: "index_classrooms_users_on_classroom_id_and_user_id", unique: true, using: :btree
   add_index "classrooms_users", ["user_id", "classroom_id"], name: "index_classrooms_users_on_user_id_and_classroom_id", unique: true, using: :btree
 
-  create_table "contexts", force: true do |t|
+  create_table "contexts", force: :cascade do |t|
     t.text     "title"
     t.text     "url"
     t.text     "provider"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 20150302221132) do
     t.text     "favicon_url"
   end
 
-  create_table "domain_admin_roles", force: true do |t|
+  create_table "domain_admin_roles", force: :cascade do |t|
     t.integer "user_id"
     t.integer "domain_id"
   end
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(version: 20150302221132) do
   add_index "domain_admin_roles", ["domain_id"], name: "index_domain_admin_roles_on_domain_id", using: :btree
   add_index "domain_admin_roles", ["user_id"], name: "index_domain_admin_roles_on_user_id", using: :btree
 
-  create_table "domains", force: true do |t|
-    t.string   "domain"
+  create_table "domains", force: :cascade do |t|
+    t.string   "domain",      limit: 255
     t.datetime "imported_at"
     t.boolean  "importing"
     t.datetime "created_at"
@@ -87,21 +87,21 @@ ActiveRecord::Schema.define(version: 20150302221132) do
 
   add_index "domains", ["domain"], name: "index_domains_on_domain", using: :btree
 
-  create_table "nodes", force: true do |t|
+  create_table "nodes", force: :cascade do |t|
     t.integer  "assignment_id"
     t.integer  "user_id"
     t.integer  "parent_id"
-    t.text     "url",                             null: false
+    t.text     "url",                                         null: false
     t.text     "title"
     t.datetime "arrived_at"
     t.datetime "departed_at"
-    t.boolean  "idle",            default: false
+    t.boolean  "idle",                        default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "x"
     t.float    "y"
-    t.integer  "rank",            default: 0
-    t.string   "temp_id"
+    t.integer  "rank",                        default: 0
+    t.string   "temp_id",         limit: 255
     t.integer  "context_id"
     t.boolean  "redirect"
     t.text     "redirected_from"
@@ -112,58 +112,59 @@ ActiveRecord::Schema.define(version: 20150302221132) do
   add_index "nodes", ["parent_id"], name: "index_nodes_on_parent_id", using: :btree
   add_index "nodes", ["user_id"], name: "index_nodes_on_user_id", using: :btree
 
-  create_table "oauth_access_grants", force: true do |t|
-    t.integer  "resource_owner_id", null: false
-    t.integer  "application_id",    null: false
-    t.string   "token",             null: false
-    t.integer  "expires_in",        null: false
-    t.text     "redirect_uri",      null: false
-    t.datetime "created_at",        null: false
+  create_table "oauth_access_grants", force: :cascade do |t|
+    t.integer  "resource_owner_id",             null: false
+    t.integer  "application_id",                null: false
+    t.string   "token",             limit: 255, null: false
+    t.integer  "expires_in",                    null: false
+    t.text     "redirect_uri",                  null: false
+    t.datetime "created_at",                    null: false
     t.datetime "revoked_at"
-    t.string   "scopes"
+    t.string   "scopes",            limit: 255
   end
 
   add_index "oauth_access_grants", ["token"], name: "index_oauth_access_grants_on_token", unique: true, using: :btree
 
-  create_table "oauth_access_tokens", force: true do |t|
+  create_table "oauth_access_tokens", force: :cascade do |t|
     t.integer  "resource_owner_id"
     t.integer  "application_id"
-    t.string   "token",             null: false
-    t.string   "refresh_token"
+    t.string   "token",             limit: 255, null: false
+    t.string   "refresh_token",     limit: 255
     t.integer  "expires_in"
     t.datetime "revoked_at"
-    t.datetime "created_at",        null: false
-    t.string   "scopes"
+    t.datetime "created_at",                    null: false
+    t.string   "scopes",            limit: 255
   end
 
   add_index "oauth_access_tokens", ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true, using: :btree
   add_index "oauth_access_tokens", ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id", using: :btree
   add_index "oauth_access_tokens", ["token"], name: "index_oauth_access_tokens_on_token", unique: true, using: :btree
 
-  create_table "oauth_applications", force: true do |t|
-    t.string   "name",                      null: false
-    t.string   "uid",                       null: false
-    t.string   "secret",                    null: false
-    t.text     "redirect_uri",              null: false
+  create_table "oauth_applications", force: :cascade do |t|
+    t.string   "name",         limit: 255,                 null: false
+    t.string   "uid",          limit: 255,                 null: false
+    t.string   "secret",       limit: 255,                 null: false
+    t.text     "redirect_uri",                             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "scopes",       default: "", null: false
+    t.string   "scopes",       limit: 255, default: "",    null: false
+    t.boolean  "trusted",                  default: false
   end
 
   add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
 
-  create_table "org_units", force: true do |t|
+  create_table "org_units", force: :cascade do |t|
     t.integer  "domain_id"
-    t.string   "name"
-    t.string   "org_unit_path"
-    t.string   "parent_org_unit_path"
+    t.string   "name",                 limit: 255
+    t.string   "org_unit_path",        limit: 255
+    t.string   "parent_org_unit_path", limit: 255
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "projects", force: true do |t|
-    t.string   "title"
+  create_table "projects", force: :cascade do |t|
+    t.string   "title",        limit: 255
     t.text     "description"
     t.integer  "classroom_id"
     t.datetime "due_at"
@@ -174,43 +175,43 @@ ActiveRecord::Schema.define(version: 20150302221132) do
 
   add_index "projects", ["classroom_id"], name: "index_projects_on_classroom_id", using: :btree
 
-  create_table "trailblazer_admins", force: true do |t|
+  create_table "trailblazer_admins", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "uid"
-    t.string   "name"
-    t.string   "email"
-    t.string   "access_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "uid",                    limit: 255
+    t.string   "name",                   limit: 255
+    t.string   "email",                  limit: 255
+    t.string   "access_token",           limit: 255
     t.datetime "token_expires"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                  default: false
-    t.boolean  "active",                 default: false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "gender"
+    t.boolean  "admin",                              default: false
+    t.boolean  "active",                             default: false
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.string   "gender",                 limit: 255
     t.text     "image"
-    t.string   "google_profile"
-    t.string   "org_unit_path"
+    t.string   "google_profile",         limit: 255
+    t.string   "org_unit_path",          limit: 255
     t.integer  "org_unit_id"
     t.integer  "domain_id"
-    t.boolean  "teacher",                default: false
-    t.string   "password_digest"
-    t.string   "reset_password_token"
+    t.boolean  "teacher",                            default: false
+    t.string   "password_digest",        limit: 255
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "last_email"
+    t.string   "last_email",             limit: 255
     t.datetime "last_confirmed_at"
   end
 
